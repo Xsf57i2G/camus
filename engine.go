4b2eaf230b6cd59523099c641ef14c676b78b09c
Xsf57i2G <fim2cmvs@protonmail.com>
2024-12-01 04:28:18 -0800

    now running update 60 times a second

--- engine.go
+++ engine.go
@@ -10,11 +10,19 @@ import (
 type Engine struct {
 	renderer *monet.Renderer
 	refresh  time.Duration
+	delta    time.Duration
 }
 
 func Run(root Node) error {
 	if err := root.Init(); err != nil {
 		return err
+	}
+	var ticker = time.NewTicker(time.Second / 60)
+	defer ticker.Stop()
+	for range ticker.C {
+		if err := root.Update(time.Second / 60); err != nil {
+			return err
+		}
 	}
 	return nil
 }
